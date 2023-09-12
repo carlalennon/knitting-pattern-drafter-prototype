989987f4da7e98dcdf5a22c007da7b23503ced7e
Your Name <you@example.com>
2023-09-12 18:31:10 +0100

    Draw knit icon to canas

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+
+let iconKnit = new Image();
+iconKnit.src = "./assets/images/icon-k.png";
+
+function createGrid() {
+    
+}
+
+function draw() {
+    ctx.drawImage(iconKnit, 0, 0);
+}
+
+function intervalLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    draw();
+}
+
+setInterval(intervalLoop,200);
+
+console.log("hi");
